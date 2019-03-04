253af3f99e7146df3e28857bb60b0d6fbebfc110
snaphappygeek <33161635+snaphappygeek@users.noreply.github.com>
2019-03-03 20:38:28 -0500

    Update hangman.c

--- hangman.c
+++ hangman.c
@@ -3,12 +3,66 @@
 // If you feel this code needs to be corrected, don't hesitate
 // to do so.
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#define HANGED 7
+
+void displayString(char* current, int stringLength)
+{
+  int i;
+
+  for (i = 0; i < stringLength; i++)
+  {
+    // If there is no element, make the thing blank
+    if (current[i] == 0)
+    {
+      printf("_");
+    }
+    //Otherwise, place the letter into the string
+    else
+    {
+      printf("%c", current[i]);
+    }
+    // Include a space between each letter
+    printf(" ");
+  }
+
+  // Have a new line for the string
+  printf("\n");
+}
+
+// Displaying the hanging body
+void displayBody(int correctLetters)
+{
+
+}
+
+// The searching prototype that Frank made
+bool checkChar(const char* answer, char* current, char guess)
+{
+  bool found = false;
+  int i;
+  int length = strlen(answer);
+
+  for (i = 0; i < length; i++)
+  {
+    if (answer[i] == guess)
+    {
+      found = true;
+      current[i] = guess;
+
+    }
+  }
+
+  return found;
+}
 
 int main(void)
 {
-  char *string = "guess";
+  // This is the
+  char *stringGuess = "guess";
+  char *stringRight;
   char input, letter;
   char alphabet[26];
   // i is the for loop, guessLen is for the length of the actual word
@@ -17,53 +71,32 @@ int main(void)
   // correctLetters is the amount of times a correct letter is found,
   // which will be added to correctGuess after going through the word.
   int i, guessLen, alphabetLetters = 0, correctGuess = 0, correctLetters = 0;
+  printf("Welcome to hangman\n");
 
-  guessLen = strlen(string);
+  guessLen = strlen(stringGuess);
+  // Dynamically Allocate the word to fit the thing itself.
+  stringRight = malloc(sizeof(char) * (guessLen + 1));
 
-  while (correctLetters <= guessLen)
+  while (correctLetters < guessLen)
   {
-    printf("\n%d out of %d found\n", correctLetters, guessLen);
+    // Display the string
+    displayString(stringRight, guessLen);
     printf("Please input a character: ");
     scanf("%c", &input);
     // This is to remove the extra newline from scanf
     getchar();
     printf("Guess: %c\n", input);
 
-    // If the input is correct, and is not a blank character
-    // we move on to the next letter.
-    // Otherwise, we put the word in an array of incorrect letters
-    for (i = 0; i < guessLen; i++)
+    if (checkChar(stringGuess, stringRight, input));
     {
-      // If the letter is correct, we increment the correct letter by one
-      // If not, we put it inside the alphabet array
-      if (input == string[i])
-      {
-        printf("Right letter\n");
-        correctGuess++;
-      }
-
-      // If there are correct letters found in the word,
-      // we update the countLetters variable.
-      if (correctGuess != 0)
-        correctLetters += correctGuess;
-      else
-      {
-        // Otherwise, we put the incorrect letter into the
-        // alphabet array.
-        for (i = 0; i < alphabetLetters; i++)
-        {
-          if (alphabet[i] == input)
-          {
-            printf("Letter %c is already taken\n", input);
-            alphabet[alphabetLetters] = input;
-            alphabetLetters++;
-          }
-        }
-      }
-
+      printf("Correct\n");
+      correctLetters++;
     }
+
   }
 
+  // Free memory from the guessed string
+  free(stringRight);
   printf("End of program");
 
   return 0;
